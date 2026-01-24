664c41ec32b3eee560f88432c99ef58d7abca9ef
Shafin98 <98sasiam@gmail.com>
2026-01-24 14:31:15 +0600

    feat: added template folder & html file

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
 
-    return HttpResponse('Hello Blog')
-    # return render(request, 'index.html')
+    
+    return render(request, 'home/home.html')
 
