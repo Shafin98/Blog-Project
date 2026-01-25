d00d3e55455bb23ec9845b4ac3c27db567dc6bd7
Shafin98 <98sasiam@gmail.com>
2026-01-25 14:39:19 +0600

    feat: added register and login page

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
 
-    
     return render(request, 'home/home.html')
+
+def register(request):
+
+
+
+    return render(request, 'auth/register.html')
+
+
+def login_view(request):
+
+
+
+    return render(request, 'auth/login.html')
 
