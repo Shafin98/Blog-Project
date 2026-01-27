77ef0a155f7ded6be631f07989df8b9aea3587c4
Shafin98 <98sasiam@gmail.com>
2026-01-27 14:36:31 +0600

    style: added base.html for other html files

--- blog/blogapp/models.py
+++ blog/blogapp/models.py
@@ -19,3 +19,6 @@ class Post(models.Model):
     description = models.TextField()
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
