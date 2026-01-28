1180b902370ffc8c04e0aa38b2a458db199e23f3
Shafin98 <98sasiam@gmail.com>
2026-01-28 15:14:07 +0600

    feat: added post view and post delete options

--- blog/blogapp/urls.py
+++ blog/blogapp/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('create_post/', views.create_post,name='create_post'),
     path('view_post/', views.view_post,name='view_post'),
     path('logout_view/', views.logout_view,name='logout_view'),
+    path('post_detail/<int:post_id>', views.post_detail,name='post_detail'),
+    path('edit_post/<int:post_id>', views.edit_post,name='edit_post'),
+    path('delete_post/<int:post_id>', views.delete_post,name='delete_post'),
 ]
--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -101,3 +101,17 @@ def view_post(request):
 
     return render(request, 'posts/my_post.html')
 
+def post_detail(request, post_id):
+
+    post = Post.objects.get(id=post_id)
+    return render(request, 'posts/post_details.html', {'post': post})
+
+
+
+@login_required
+def delete_post(request, post_id):
+
+    post = Post.objects.get(id=post_id)
+    post.delete()
+    return HttpResponse("Task deleted successfully.")
+    
